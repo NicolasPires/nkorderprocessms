c4150142599f1b5ecc60973e7a095cda7529913e
Nicolas Pires <nicolask.pires@gmail.com>
2026-02-03 17:40:25 -0300

    chore: initial commit

---
+++ src/test/java/com/nksolucoes/nkorderprocessms/NkorderprocessmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nksolucoes.nkorderprocessms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NkorderprocessmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
